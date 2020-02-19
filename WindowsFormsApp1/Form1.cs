40ad6c5f4fae7abd7b9528ef14c805d9b2aa68fe
梦凡 李 <27570051@qq.com>
2020-02-19 17:13:20 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            double a = 0;
+            double b = 0;
+            if (double.TryParse(textBox1.Text, out a)) { }
+            else
+            {
+                label1.Text = "请在第一个框内输入数字";
+                return;
+            }
+            if(double.TryParse(textBox3.Text, out b)) { }
+            else
+            {
+                label1.Text = "请在第二个框内输入数字";
+                return;
+            }
+            double c = 0;
+            string operate = textBox2.Text;
+            switch (operate)
+            {
+                case "+":
+                    c = a + b;
+                    label1.Text = $"{c}";
+                    break;
+                case "-":
+                    c = a - b;
+                    label1.Text = $"{c}";
+                    break;
+                case "*":
+                    c = a * b;
+                    label1.Text = $"{c}";
+                    break;
+                case "/":
+                    if (b == 0) label1.Text = "被除数不能为0";
+                    else {
+                        c = a / b;
+                        label1.Text = $"{c}";
+                    }
+                    break;
+                case "%":
+                    c = a % b;
+                    label1.Text = $"{c}";
+                    break;
+                default:
+                    label1.Text = "符号位出错";
+                    break;
+            }
+        }
+
+        
+        }
+    }
---
+++ calculator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; 
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                   
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+           
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+               
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+              
+                Console.Write("输入第一个数字 ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("运算错误 请重新输入 ");
+                    numInput1 = Console.ReadLine();
+                }
+
+               
+                Console.Write("输入第二个数字 ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("运算错误 请重新输入 ");
+                    numInput2 = Console.ReadLine();
+                }
+
+               
+                Console.WriteLine("选择运算符号");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.Write("你的选择？ ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("运算错误\n");
+                    }
+                    else Console.WriteLine("你的结果: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                
+                Console.Write("按 'n' 关闭此程序, 按任一键继续运行 ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); 
+            }
+            return;
+        }
+    }
+}
