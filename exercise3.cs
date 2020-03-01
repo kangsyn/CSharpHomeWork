42854b5c5044fa4e9fffb31697054faa52c076dc
kangsyn <61226983+kangsyn@users.noreply.github.com>
2020-03-01 19:36:25 +0800

    Add files via upload

---
+++ exercise3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+ static  void Main(string[] args)
+           {
+            int[] nums = new int[101];
+            for (int i = 2; i < 101; i++)
+            {
+                nums[i] = i;
+            }
+            for (int i = 2; i < 101; i++)
+            {
+                for (int j = 2; i * j < 101; j++)
+                {
+                    nums[i * j] = 0;
+                }
+            }
+                for (int i = 2; i < 101; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
+
+        }
+    }
+}
