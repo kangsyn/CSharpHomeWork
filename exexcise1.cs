b505237214b79264145ac6d1b1deb68001cd0b8b
kangsyn <61226983+kangsyn@users.noreply.github.com>
2020-03-01 19:35:06 +0800

    Add files via upload

---
+++ exexcise1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("请输入一个大于1的整数：");
+                string str = Console.ReadLine();
+                if (int.TryParse(str, out int n) && n > 1)
+                {
+                    Console.Write($"{n}的素数因子有：");
+                    for (int i = 2; i <= n; i++)
+                    {
+                        int k = n % i;
+                        if (Judgement(i) && k == 0) Console.Write($"{i} ");
+                    }
+                    return;
+
+                }
+                else Console.Write("输入错误，请重新输入：");
+            }
+        }
+        static bool Judgement(int n)
+        {
+            for (int i = 2; i < n - 1; i++)
+            {
+                int k = n % i;
+                if (k == 0) return false;
+            }
+            return true;
+        }
+    }
+}
